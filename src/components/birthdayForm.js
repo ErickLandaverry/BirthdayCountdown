de68a08ed4d44bfb74db9ac39a3c83673d45ba8e
landy <landy.erick20@gmail.com>
2018-04-02 16:17:50 -0600

    added our generate button

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -7,19 +7,29 @@ class BirthdayForm extends Component {
         super(props);
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleGenerate = this.handleGenerate.bind(this)
+        
+        
 
         this.state = {
-            startDate: moment()
+            startDate: moment(),
+            formCompleted: false
         }
     }
 
     handleChange(date) {
-
-        this.setState({
+            this.setState({
             startDate: date
         })
     }
 
+    handleGenerate() {
+        this.setState({
+             formCompleted: true
+        })
+    }
+
+    
     render() {
         return (
             <div>
@@ -28,7 +38,7 @@ class BirthdayForm extends Component {
                     selected={this.state.startDate}
                     onChange={this.handleChange}
                 />
-                    
+                <a onClick={this.handleGenerate}>Generate Countdown</a>
             </div>
         )
     }
